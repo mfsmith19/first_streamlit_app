bca68aa0104b57c080cfd329a7270f6536279281
mfsmith19 <98363830+mfsmith19@users.noreply.github.com>
2022-07-27 16:05:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Streamlit challenge 1
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUE (" + add_my_fruit + ")")
